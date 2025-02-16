84ad837dfb67fc078e990e17e122481ee6451269
faizanalim <faizan.23@hotmail.com>
2025-02-16 05:45:43 +0500

    Create UI and Nullable false

---
+++ Villa_API/Migrations/20250216004140_ChangeNullabletofalse.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Villa_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeNullabletofalse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Villas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 2, 16, 5, 41, 39, 491, DateTimeKind.Local).AddTicks(5618));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 2, 16, 5, 41, 39, 491, DateTimeKind.Local).AddTicks(5631));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 2, 16, 5, 41, 39, 491, DateTimeKind.Local).AddTicks(5633));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 2, 16, 5, 41, 39, 491, DateTimeKind.Local).AddTicks(5635));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2025, 2, 16, 5, 41, 39, 491, DateTimeKind.Local).AddTicks(5636));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Villas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 2, 12, 8, 57, 46, 353, DateTimeKind.Local).AddTicks(7583));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 2, 12, 8, 57, 46, 353, DateTimeKind.Local).AddTicks(7596));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 2, 12, 8, 57, 46, 353, DateTimeKind.Local).AddTicks(7598));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 2, 12, 8, 57, 46, 353, DateTimeKind.Local).AddTicks(7600));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2025, 2, 12, 8, 57, 46, 353, DateTimeKind.Local).AddTicks(7602));
+        }
+    }
+}
