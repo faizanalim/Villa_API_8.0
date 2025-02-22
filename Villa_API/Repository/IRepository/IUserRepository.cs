e482cb575ae7d03130bf5a6701e6ec6e69bf6ed2
faizanalim <faizan.23@hotmail.com>
2025-02-22 07:15:48 +0500

    Add User Repo

---
+++ Villa_API/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Villa_API.Models.Dto;
+using Villa_API.Models;
+
+namespace Villa_API.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+
+
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+
+
+        Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO);
+    }
+}
---
+++ Villa_API/Repository/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using Villa_API.Data;
+using Villa_API.Models;
+using Villa_API.Models.Dto;
+using Villa_API.Repository.IRepository;
+
+namespace Villa_API.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool IsUniqueUser(string username)
+        {
+            var user = _db.LocalUsers.FirstOrDefault(x => x.UserName == username);
+            if (user == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
+        {
+            LocalUser user = new()
+            {
+                UserName = registerationRequestDTO.UserName,
+                Password = registerationRequestDTO.Password,
+                Name = registerationRequestDTO.Name,
+                Role = registerationRequestDTO.Role
+            };
+            _db.LocalUsers.Add(user);
+            await _db.SaveChangesAsync();
+            user.Password = "";
+            return user;
+        }
+    }
+}
